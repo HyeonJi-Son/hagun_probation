ee330fc8db4ce8f6afc3aa8b96859dff02dd2e76
Hyeonji-Son <sustka46@gmail.com>
2023-06-28 20:34:50 +0900

    [230628]api 구현, cors npm 설치

--- joCoding_clone/fourth_figlet/index.js
+++ joCoding_clone/fourth_figlet/index.js
@@ -1,3 +1,5 @@
+/* -----figlet 사용----- */
+
 // console.log("Hello world!")
 
 // var figlet = require("figlet");
@@ -11,14 +13,72 @@
 //   console.log(data);
 // });
 
+/* -----expres 설치----- */
+
 const express = require('express')
+var cors = require('cors')
+
 const app = express()
 const port = 3000;
+
+app.use(cors())
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example application listening on port ${port}`)
+// })
+
+/* -----동물 소리 API 구현하기 ----- */
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/dog', function (req, res) {
+  res.send("<a href='https://www.youtube.com/watch?v=1NRp0FyJO1Q&list=PLU9-uwewPMe0sQkZlOhnhl8MRb_uFJBID&index=26'>조코딩 유튜브</a>")
+})
+
+app.get('/cat', function (req, res) {
+  res.json({'sound' : 'Meow'})
+})
+
+app.get('/sound/:name', function (req, res) {
+  const { name } = req.params
+
+  if(name == "dog"){
+    res.json({'sound' : '멍멍'})
+  }else if(name == "cat"){
+    res.json({'sound' : '야옹'})
+  }else if(name == "pig"){
+    res.json({'sound' : '꿀꿀'})
+  }else {
+    res.json({'sound' : '알수없음'})
+  }
+
+})
+
+
 app.listen(port, () => {
     console.log(`Example application listening on port ${port}`)
 })
+
+app.get('/user/:id', (req, res) => {
+    const q = req.params;
+    console.log(q);
+
+})
+
+app.get('/user/:id', (req, res) => {
+  // const q = req.params;
+  // console.log(q);
+
+  const q = req.query;
+  console.log(q)
+
+  res.json({'userid': q.id})
+
+})
+
