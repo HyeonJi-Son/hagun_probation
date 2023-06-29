fb8904afb6b4cf5ac58e8899c66b28ffb8126649
Hyeonji-Son <sustka46@gmail.com>
2023-06-29 21:37:17 +0900

    [230629]조코딩 -  코딩 기초와 웹 프로그래밍(풀스택) 완강

--- joCoding_clone/fifth_ejs/index.js
+++ joCoding_clone/fifth_ejs/index.js
@@ -36,3 +36,49 @@ app.post('/create', function(req, res) {
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
+
+/*
+SQL lite
+- RDB(Relational Database) 의 한 종류다.
+
+CRUD (Create Read Update Delete)
+
+*/
+
+app.get('/', async function(req, res) {
+  const comments = await Comments.findAll();
+
+  res.render('index', { comments: comments});
+});
+
+app.post('/create', async function(req, res) {
+  console.log(req.body);
+
+  const { content } = req.body; //request된 객체 중 body에 담긴 내역을 content라는 객체에 담아라.
+  await Comments.create({content: content});
+  //Comments라는 table에 생성할 것이다. 무엇을? 새 정보를.
+    //content라는 컬럼에 변수 content의 값을.
+
+  res.redirect('/');
+});
+
+app.post('/update/:id', async function(req, res) {
+  console.log(req.params);
+  console.log(req.body);
+
+  const { content } = req.body;
+  res.redirect('/');
+});
+
+app.post('/delete/:id', async function(req, res) {
+  console.log(req.params);
+  const { id } = req.params
+  
+  await Comments.destroy({
+    where: {
+      id: id
+    }
+  });
+
+  res.redirect('/')
+});
